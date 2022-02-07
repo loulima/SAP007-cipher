df145723b8833844e31cb891dab967dfd18112f3
loulima <louizalima13@gmail.com>
2022-02-07 18:54:43 -0300

    terceiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,58 @@
 const cipher = {
-  // ...
+  encode: (offset, texto) => {
+    if (
+      Number(texto) ||
+      texto == "" ||
+      texto == null ||
+      offset == "" ||
+      offset == null
+    ) {
+      throw new TypeError("ERRO:", "cipher.js", 4);
+    }
+    let textCipher = "";
+    let letters = texto.length;
+
+    for (let i = 0; i < letters; i++) {
+      let asciiNum = texto.charCodeAt(i); // pega o codigo corresnpondente a tabela ASC referente as letras digitadas
+      const codeAscii = 65; //corresponde a letra A na tabela ASCII
+
+      if (asciiNum >= 65 && asciiNum <= 90) {
+        // retorna apenas o alfabeto maiúsculo
+        let encodeValue = ((asciiNum + codeAscii + offset) % 26) + codeAscii; //calculo para cifrar
+        textCipher = textCipher + String.fromCharCode(encodeValue); //faz o contrario de charcodeat e ao inves de retornar o valor da tabela ascii retorna a letra
+      } else {
+        textCipher = textCipher + String.fromCharCode(asciiNum); //ele serve para acrescentar algo que não seja modificado
+      }
+    }
+    return textCipher;
+  },
+
+  decode: (offset, texto) => {
+    if (
+      Number(texto) ||
+      texto == "" ||
+      texto == null ||
+      offset == "" ||
+      offset == null
+    ) {
+      throw new TypeError("ERRO:", "cipher.js", 4);
+    }
+    let textDecipher = "";
+    let letters = texto.length;
+
+    for (let i = 0; i < letters; i++) {
+      let asciiNum = texto.charCodeAt(i);
+      const codeAscii = 90;
+      if (asciiNum <= 90 && asciiNum >= 65) {
+        let decodeValue = ((asciiNum - codeAscii - offset) % 26) + codeAscii; //calculo p/ decifrar
+        textDecipher = textDecipher + String.fromCharCode(decodeValue);
+      } else {
+        textDecipher = textDecipher + String.fromCharCode(asciiNum);
+      }
+    }
+
+    return textDecipher;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById('btnEncode').addEventListener("click", cifrar);
+document.getElementById('btnDecode').addEventListener("click", decifrar);
+
+function cifrar(e) {
+  e.preventDefault();
+  let texto = document.getElementById("message").value.toUpperCase(); //ler a mensagem do textarea e por em MAIUSCULA
+  let offset = Number(document.getElementById("offset").value); //identifica o deslocamento selecionado
+  document.getElementById("txtResult").value = cipher.encode(offset, texto); //colocar o valor da função cipher junto com a mensagem da textarea
+}
+
+function decifrar (e) {
+  e.preventDefault();
+  let texto = document.getElementById("message").value.toUpperCase();
+  let offset = Number(document.getElementById("offset").value);
+  
+      document.getElementById("txtResult").value=cipher.decode(offset, texto);
+    }
+  
