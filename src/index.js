f92eb181eece44b0281838fd5fa096c2e281630c
Louiza Lima <72285598+loulima@users.noreply.github.com>
2022-02-08 10:23:54 -0300

    ajuste formatacao

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ document.getElementById('btnDecode').addEventListener("click", decifrar);
 
 function cifrar(e) {
   e.preventDefault();
-  let texto = document.getElementById("message").value.toUpperCase(); //ler a mensagem do textarea e por em MAIUSCULA
-  let offset = Number(document.getElementById("offset").value); //identifica o deslocamento selecionado
-  document.getElementById("txtResult").value = cipher.encode(offset, texto); //colocar o valor da função cipher junto com a mensagem da textarea
+  let texto = document.getElementById("message").value.toUpperCase(); 
+  let offset = Number(document.getElementById("offset").value); 
+  document.getElementById("txtResult").value = cipher.encode(offset, texto); 
 }
 
 function decifrar (e) {
