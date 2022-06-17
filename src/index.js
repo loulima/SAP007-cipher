51a36d59b867931b09760d22e9588a249d8ba07f
loulima <louizalima13@gmail.com>
2022-06-17 20:22:56 -0300

    ajustes finais

--- src/index.js
+++ src/index.js
@@ -3,18 +3,28 @@ import cipher from './cipher.js';
 document.getElementById('btnEncode').addEventListener("click", cifrar);
 document.getElementById('btnDecode').addEventListener("click", decifrar);
 
+document.getElementById('copy-encode-message').addEventListener("click", copy);
+
+function copy(e){
+  e.preventDefault();
+  let copyText= document.getElementById('txtResult');
+ copyText.select();
+  navigator.clipboard.writeText(copyText.value);
+  console.log(copyText.value );
+}
+
 function cifrar(e) {
   e.preventDefault();
-  let texto = document.getElementById("message").value.toUpperCase(); 
+  let text = document.getElementById("message").value.toUpperCase(); 
   let offset = Number(document.getElementById("offset").value); 
-  document.getElementById("txtResult").value = cipher.encode(offset, texto); 
+  document.getElementById("txtResult").value = cipher.encode(offset, text); 
 }
 
 function decifrar (e) {
   e.preventDefault();
-  let texto = document.getElementById("message").value.toUpperCase();
-  let offset = Number(document.getElementById("offset").value);
+  let text = document.getElementById("decode-message").value.toUpperCase();
+  let offset = Number(document.getElementById("decode-offset").value);
   
-      document.getElementById("txtResult").value=cipher.decode(offset, texto);
+      document.getElementById("decode-result").value=cipher.decode(offset, text);
     }
   
