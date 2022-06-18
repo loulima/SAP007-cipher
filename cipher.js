65423a2d213cefd6b44c948f1f0f16845730a5a0
loulima <louizalima13@gmail.com>
2022-06-17 21:04:07 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,18 +1,17 @@
 const cipher = {
 
-  encode: (offset, texto) => { 
-  if (Number(texto) || texto == "" || texto == null || offset == "" || offset == null) {
+  encode: (offset, text) => { 
+  if (Number(text) || text == "" || text == null || offset == "" || offset == null) {
       throw new TypeError("ERRO:", "cipher.js", 4);
     }
     let textCipher = "";
-    let letters = texto.length;
+    let letters = text.length;
 
     for (let i = 0; i < letters; i++) {
-      let asciiNum = texto.charCodeAt(i); 
+      let asciiNum = text.charCodeAt(i); 
       const codeAscii = 65;
 
       if (asciiNum >= 65 && asciiNum <= 90) {
-        // retorna apenas o alfabeto maiúsculo
         let encodeValue = ((asciiNum + codeAscii + offset) % 26) + codeAscii;
         textCipher = textCipher + String.fromCharCode(encodeValue); 
       } else {
@@ -22,16 +21,17 @@ const cipher = {
     return textCipher;
   },
 
-  decode: (offset, texto) => {
-    if (Number(texto) || texto == "" || texto == null || offset == "" || offset == null) {
+  decode: (offset, text) => {
+    if (Number(text) || text == "" || text == null || offset == "" || offset == null) {
       throw new TypeError("ERRO:", "cipher.js", 4);
     }
     let textDecipher = "";
-    let letters = texto.length;
+    let letters = text.length;
 
     for (let i = 0; i < letters; i++) {
-      let asciiNum = texto.charCodeAt(i);
+      let asciiNum = text.charCodeAt(i);
       const codeAscii = 90;
+      
       if (asciiNum <= 90 && asciiNum >= 65) {
         let decodeValue = ((asciiNum - codeAscii - offset) % 26) + codeAscii; 
         textDecipher = textDecipher + String.fromCharCode(decodeValue);
